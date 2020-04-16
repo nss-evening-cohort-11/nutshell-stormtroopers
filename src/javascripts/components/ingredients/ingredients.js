4cc7163938ca545c9511fed67b29aaae9c8ac1af
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-04-15 21:04:51 -0500

    got the ingredients to print to the page

--- src/javascripts/components/ingredients/ingredients.js
+++ src/javascripts/components/ingredients/ingredients.js
@@ -1,8 +1,25 @@
+import ingredientsData from '../../helpers/data/ingredientsData';
 import utils from '../../helpers/utils';
 
 const buildIngredientsSection = () => {
-  const domString = '<h2>Ingredients</h2>';
-  utils.printToDom('ingredients-section', domString);
+  ingredientsData.getIngredients()
+    .then((response) => {
+      const ingredients = response;
+      let domString = '<h2>Ingredients</h2>';
+      ingredients.forEach((ingredient) => {
+        domString += '<div class="card">';
+        domString += `<p>${ingredient.name}</p>`;
+        domString += `<img class="ingredient-image" src="${ingredient.imageUrl}">`;
+        domString += `<p>${ingredient.type}</p>`;
+        domString += `<p>${ingredient.cost}</p>`;
+        domString += `<p>${ingredient.quantity}</p>`;
+        domString += `<p>${ingredient.size}</p>`;
+        domString += `<p>${ingredient.unit}</p>`;
+        domString += '</div>';
+      });
+      utils.printToDom('ingredients-section', domString);
+    })
+    .catch((err) => console.error('Could not get ingredients', err));
   $('#home-page').addClass('hide');
   $('#staff-section').addClass('hide');
   $('#reservations-section').addClass('hide');
