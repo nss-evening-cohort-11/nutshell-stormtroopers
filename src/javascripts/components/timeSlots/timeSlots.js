b410251bbf0b3f91b545037cdc4691a1ba532705
delindell <daviselindell@gmail.com>
2020-04-20 08:17:20 -0500

    another small style change

--- src/javascripts/components/timeSlots/timeSlots.js
+++ src/javascripts/components/timeSlots/timeSlots.js
@@ -5,8 +5,8 @@ const buildTimeSlots = (timeSlots) => {
       // time reserved
       timeSlot.reservations.forEach((reservation) => {
         domString += `<li class="list-group-item" id="${timeSlot.id}">`;
-        domString += `<div class="d-flex justify-content-between"><span class="reserved-timeslot text-center"><strong>(Reserved Time)</strong> ${timeSlot.time}</span>`;
-        domString += `<button class="edit-reservation-button" id="${reservation.id}">View Reservation</button>`;
+        domString += `<div class="d-flex justify-content-between"><span class="reserved-timeslot"><strong>(Reserved Time)</strong> ${timeSlot.time}</span>`;
+        domString += `<button class="edit-reservation-button" id="${reservation.id}">${reservation.partyName}</button>`;
         domString += `<button class="delete-reservation-button" id="${reservation.id}"><i class="far fa-times-circle"></i></button>`;
         domString += '</div>';
         domString += '</li>';
