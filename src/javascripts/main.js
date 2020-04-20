49d536997a4fa72475cebae2a0563a70309f593b
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-04-20 17:01:17 -0500

    auth buttons work now, and fixed conflict

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -7,15 +7,17 @@ import home from './components/home/home';
 import staff from './components/staff/staff';
 import reservations from './components/reservations/reservations';
 import ingredients from './components/ingredients/ingredients';
-import menuComponent from './components/menu/menu';
+import menu from './components/menu/menu';
 import authData from './helpers/data/authData';
 
 const navbarClickEvents = () => {
-  $('#brand-logo').click(home.showHomePage);
-  $('#staff-button').click(staff.staffInit);
-  $('#reservations-button').click(reservations.buildReservationsSection);
-  $('#menu-button').on('click', menuComponent.buildMenuSection);
-  $('#ingredients-button').click(ingredients.buildIngredientsSection);
+  $(document).ready(() => {
+    $('#brand-logo').click(home.showHomePage);
+    $('#staff-button').click(staff.staffInit);
+    $('#reservations-button').click(reservations.buildReservationsSection);
+    $('#menu-button').click(menu.buildMenuSection);
+    $('#ingredients-button').click(ingredients.buildIngredientsSection);
+  });
 };
 
 const init = () => {
