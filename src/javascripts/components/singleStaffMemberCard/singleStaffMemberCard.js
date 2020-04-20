a663ebb97cbfd492fd5a1b95995f12bbf503c501
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-04-20 17:54:01 -0500

    fixed filter items and staff photo overflow

--- src/javascripts/components/singleStaffMemberCard/singleStaffMemberCard.js
+++ src/javascripts/components/singleStaffMemberCard/singleStaffMemberCard.js
@@ -4,7 +4,7 @@ const buildSingleStaffMemberCard = (staffMember, thisEmployeeJob) => {
   let domString = '';
   domString += '<div class="col-sm-12 col-md-12 col-lg-4 col-xl-6 staff-card-divider">';
   domString += `    <div id="${staffMember.id}" data-job-id="${staffMember.jobId}" class="p-3 staff-card text-center card">`;
-  domString += `        <img class="m-3 mx-auto d-block staff-image image-fluid" src="${staffMember.imageUrl}" alt="${staffMember.name}"/>`;
+  domString += `        <div class="img-container"><img class="m-3 mx-auto d-block staff-image image-fluid" src="${staffMember.imageUrl}" alt="${staffMember.name}"/></div>`;
   domString += '        <div class="staff-card-body card-body">';
   domString += `            <h2 class="card-title">${staffMember.name}</h2>`;
   domString += `            <p class="card-text">${thisEmployeeJob.jobType}</p>`;
