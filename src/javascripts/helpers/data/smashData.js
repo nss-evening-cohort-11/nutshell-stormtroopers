f007bfdab9b21e52750d17122c536e61b575ad78
John Fleming <johndeclanfleming@gmail.com>
2020-04-28 18:49:06 -0500

    can filter reservations page by date

--- src/javascripts/helpers/data/smashData.js
+++ src/javascripts/helpers/data/smashData.js
@@ -2,14 +2,15 @@ import tableData from './tableData';
 import timeSlotData from './timeSlotData';
 import reservationData from './reservationData';
 
-const getTablesWithReservations = () => new Promise((resolve, reject) => {
+const getTablesWithReservations = (selectedDate) => new Promise((resolve, reject) => {
   tableData.getTables().then((tables) => {
     timeSlotData.getTimeSlots().then((timeSlots) => {
       const finalTables = [];
       reservationData.getReservations().then((reservationsResponse) => {
+        const todaysReservations = reservationsResponse.filter((x) => x.date === selectedDate.toString());
         tables.forEach((table) => {
           const newTable = { ...table };
-          const tableReservations = reservationsResponse.filter((x) => x.tableId === table.id);
+          const tableReservations = todaysReservations.filter((x) => x.tableId === table.id);
           newTable.timeSlots = timeSlots;
           const newTimeSlot = [];
           timeSlots.forEach((oneTimeSlot) => {
