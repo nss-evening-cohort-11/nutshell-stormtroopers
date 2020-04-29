6979d3d6837b529859bd2b6d0e860c2a8846769e
John Fleming <johndeclanfleming@gmail.com>
2020-04-28 21:43:11 -0500

    smash function returns reservations and timeslots

--- src/javascripts/components/reservationsPortal/reservationsPortal.js
+++ src/javascripts/components/reservationsPortal/reservationsPortal.js
@@ -1,11 +1,35 @@
 import utils from '../../helpers/utils';
 import './reservationsPortal.scss';
+import smashData from '../../helpers/data/smashData';
+// import timeSlotData from '../../helpers/data/timeSlotData';
+
+const buildReservationsTable = (reservations) => {
+  let domString = '';
+  reservations.forEach((res) => {
+    domString += '<div>';
+    domString += `<p>${res.partyName} & ${res.timeslot} & ${res.date}</p>`;
+    domString += '</div>';
+  });
+  utils.printToDom('filtered-reservations-container', domString);
+};
+
+const showReservationsByDate = () => {
+  const selectedDate = '2020-04-22';
+  smashData.getReservationTimeslotsByDate(selectedDate)
+    .then((reservations) => {
+      console.error('today', reservations);
+      console.error('today', reservations[0]);
+      buildReservationsTable(reservations);
+    })
+    .catch((err) => console.error('could not get reservations by date', err));
+};
 
 const buildReservationsPortalSection = () => {
   let domString = '<h1 id="reservations-portal-header" class="reservations-portal-title text-center">Reservations Portal</h1>';
   domString += '<div class="col-12 text-center"><input type="date" id="reservation-date-selector" class="mx-1" value="2020-04-21"><button type="submit" id="filter-date-btn" class="btn btn-secondary mx-1">Select Date</button></div>';
   domString += '<div id="filtered-reservations-container"></div>';
   utils.printToDom('reservations-portal-section', domString);
+  showReservationsByDate();
   $(document).ready(() => {
     $('#home-page').addClass('hide');
     $('#staff-section-container').addClass('hide');
