517bc09fd0faa661551c7e2b6f772bf867713f59
Joey Petrone <joey.petrone@gmail.com>
2020-04-28 20:54:54 -0500

    all categories print to orders page

--- src/javascripts/components/orders/orders.js
+++ src/javascripts/components/orders/orders.js
@@ -3,9 +3,10 @@ import ordersData from '../../helpers/data/ordersData';
 import utils from '../../helpers/utils';
 
 import './orders.scss';
+import menuData from '../../helpers/data/menuData';
 
 const editOrdersPage = () => {
-  const reservationId = 'reservation1';
+  const reservationId = 'reservation5';
   let domString = '';
 
   domString += '<strong><h1 id="orders-page-header" class="text-center display-4">Reservation Orders</h1></strong>';
@@ -13,33 +14,133 @@ const editOrdersPage = () => {
   // RESRVATION //
   reservationData.getReservationById(reservationId)
     .then((reservation) => {
-      domString += `<h2 id="orders-page-reservation-header" class="text-center mt-4">${reservation.partyName}</h2>`;
+      domString += `<h2 id="reservation-orders-page-header" class="text-center mt-4">${reservation.partyName}</h2>`;
 
       // ALL ORDERS //
       ordersData.getAllOrders()
         .then((orders) => {
-          const reservationOrders = orders.filter((order) => order.reservationId === reservationId);
-          domString += '<div class="d-flex justify-content-center">';
+          menuData.getAllMenuItems()
+            .then((menuItems) => {
+              const reservationOrders = orders.filter((order) => order.reservationId === reservationId);
+              // MENU ARRAY VARIABLES //
+              const menuBeverages = menuItems.filter((menuItem) => menuItem.category === 'beverage');
+              const menuAppetizers = menuItems.filter((menuItem) => menuItem.category === 'appetizer');
+              const menuSalads = menuItems.filter((menuItem) => menuItem.category === 'salad');
+              const menuMainDishes = menuItems.filter((menuItem) => menuItem.category === 'main');
+              const menuDesserts = menuItems.filter((menuItem) => menuItem.category === 'dessert');
 
-          domString += '<div id="edit-orders-container" class="col-9">';
+              domString += '<div class="d-flex justify-content-center">';
+              domString += '<div id="edit-orders-container" class="col-9">';
 
-          domString += '<h3 id="orders-page-reservation-header" class="mt-4">Orders</h3>';
-          domString += '<div id="reservation-orders-container" class="d-flex flex-row border p-4">';
+              // RESRVATION ORDERS //
+              domString += '<h3 id="reservation-orders-page-header" class="mt-4">Orders</h3>';
+              domString += '<div id="reservation-orders-container" class="row flex-row flex-nowrap m-0 p-3 border overflow-auto">';
 
+              reservationOrders.forEach((order) => {
+                const orderMenuItem = menuItems.find((menuItem) => menuItem.id === order.menuItemId);
+                domString += '<div class="card mr-2 col-3">';
+                domString += `<img class="card-img-top" src="${orderMenuItem.imageUrl}" alt="Card image cap">`;
+                domString += '<div class="card-body text-center d-flex flex-column">';
+                domString += `<h5 class="card-title text-left">${orderMenuItem.name}</h5>`;
+                domString += `<p class="card-text text-left">${orderMenuItem.description}</p>`;
+                domString += '<button class="btn btn-danger mt-auto remove-from-order-button">Remove</button>';
+                domString += '</div>';
+                domString += '</div>';
+              });
 
-          // RESRVATION ORDERS //
-          reservationOrders.forEach((order) => {
-            domString += '<div class="card mr-2 p-3">';
-            domString += `<div class="card-title">${order.menuItemId}</div>`;
-            domString += `<p class="card-text">${order.menuItemId}</p>`;
-            domString += '</div>';
-          });
+              domString += '</div>';
 
-          domString += '</div>';
-          domString += '</div>';
-          domString += '</div>';
+              // BEVERAGES //
+              domString += '<h3 id="reservation-orders-page-header" class="mt-4">Beverages</h3>';
+              domString += '<div id="reservation-orders-container" class="row flex-row flex-nowrap m-0 p-3 border overflow-auto">';
 
-          utils.printToDom('orders-section', domString);
+              menuBeverages.forEach((beverage) => {
+                domString += '<div class="card mr-2 col-3">';
+                domString += `<img class="card-img-top" src="${beverage.imageUrl}" alt="Card image cap">`;
+                domString += '<div class="card-body text-center d-flex flex-column">';
+                domString += `<h5 class="card-title text-left">${beverage.name}</h5>`;
+                domString += `<p class="card-text text-left">${beverage.description}</p>`;
+                domString += '<button class="btn btn-primary mt-auto add-to-order-button">Add To Order</button>';
+                domString += '</div>';
+                domString += '</div>';
+              });
+
+              domString += '</div>';
+
+              // APPETIZERS //
+              domString += '<h3 id="reservation-orders-page-header" class="mt-4">Appetizers</h3>';
+              domString += '<div id="reservation-orders-container" class="row flex-row flex-nowrap m-0 p-3 border overflow-auto">';
+
+              menuAppetizers.forEach((appetizer) => {
+                domString += '<div class="card mr-2 col-3">';
+                domString += `<img class="card-img-top" src="${appetizer.imageUrl}" alt="Card image cap">`;
+                domString += '<div class="card-body text-center d-flex flex-column">';
+                domString += `<h5 class="card-title text-left">${appetizer.name}</h5>`;
+                domString += `<p class="card-text text-left">${appetizer.description}</p>`;
+                domString += '<button class="btn btn-primary mt-auto">Add To Order</button>';
+                domString += '</div>';
+                domString += '</div>';
+              });
+
+              domString += '</div>';
+
+              // SALADS //
+              domString += '<h3 id="reservation-orders-page-header" class="mt-4">Salads</h3>';
+              domString += '<div id="reservation-orders-container" class="row flex-row flex-nowrap m-0 p-3 border overflow-auto">';
+
+              menuSalads.forEach((salad) => {
+                domString += '<div class="card mr-2 col-3">';
+                domString += `<img class="card-img-top" src="${salad.imageUrl}" alt="Card image cap">`;
+                domString += '<div class="card-body text-center d-flex flex-column">';
+                domString += `<h5 class="card-title text-left">${salad.name}</h5>`;
+                domString += `<p class="card-text text-left">${salad.description}</p>`;
+                domString += '<button class="btn btn-primary mt-auto">Add To Order</button>';
+                domString += '</div>';
+                domString += '</div>';
+              });
+
+              domString += '</div>';
+
+              // Main Dishes //
+              domString += '<h3 id="reservation-orders-page-header" class="mt-4">Main Dishes</h3>';
+              domString += '<div id="reservation-orders-container" class="row flex-row flex-nowrap m-0 p-3 border overflow-auto">';
+
+              menuMainDishes.forEach((mainDish) => {
+                domString += '<div class="card mr-2 col-3">';
+                domString += `<img class="card-img-top" src="${mainDish.imageUrl}" alt="Card image cap">`;
+                domString += '<div class="card-body text-center d-flex flex-column">';
+                domString += `<h5 class="card-title text-left">${mainDish.name}</h5>`;
+                domString += `<p class="card-text text-left">${mainDish.description}</p>`;
+                domString += '<button class="btn btn-primary mt-auto">Add To Order</button>';
+                domString += '</div>';
+                domString += '</div>';
+              });
+
+              domString += '</div>';
+
+              // Desserts //
+              domString += '<h3 id="reservation-orders-page-header" class="mt-4">Desserts</h3>';
+              domString += '<div id="reservation-orders-container" class="row flex-row flex-nowrap m-0 p-3 border overflow-auto">';
+
+              menuDesserts.forEach((dessert) => {
+                domString += '<div class="card mr-2 col-3">';
+                domString += `<img class="card-img-top" src="${dessert.imageUrl}" alt="Card image cap">`;
+                domString += '<div class="card-body text-center d-flex flex-column">';
+                domString += `<h5 class="card-title text-left">${dessert.name}</h5>`;
+                domString += `<p class="card-text text-left">${dessert.description}</p>`;
+                domString += '<button class="btn btn-primary mt-auto">Add To Order</button>';
+                domString += '</div>';
+                domString += '</div>';
+              });
+
+              domString += '</div>';
+
+              domString += '</div>';
+              domString += '</div>';
+
+              utils.printToDom('orders-section', domString);
+            })
+            .catch((err) => console.error('problem with get all menu items in edit orders page', err));
         })
         .catch((err) => console.error('problem with get orders by reservation id in edit orders page', err));
     })
