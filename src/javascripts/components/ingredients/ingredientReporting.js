f729db3647c880b30ffb31a8db54c233f80289a7
Aaron Lain <aaron.j.lain@gmail.com>
2020-04-30 17:02:27 -0500

    added comments lol

--- src/javascripts/components/ingredients/ingredientReporting.js
+++ src/javascripts/components/ingredients/ingredientReporting.js
@@ -3,29 +3,32 @@ import utils from '../../helpers/utils';
 import smash from '../../helpers/data/smashData';
 
 const getIngredientsReport = () => new Promise((resolve, reject) => {
+  // First let's establish some variables / Ugliness is next to godliness
   const date1 = $('#date1').val();
   const date2 = $('#date2').val();
   const modifiedDate1 = moment(date1).format('YYYY/MM/DD');
   const modifiedDate2 = moment(date2).format('YYYY/MM/DD');
-  let domString = '';
+  const names = [];
   const flatNames = [];
   const counts = {};
+  let domString = '';
+  // Now lets get our Ingredients for the date range
   smash.getIngredientsForDateRange(modifiedDate1, modifiedDate2).then((results) => {
+    // Now let's iterate through the results of the function, flatten each array, push the ingredient names
+    // into a new array of objects called [names], which we will then flatten into a regular array [newNames]
+    // then we will flatten that array yet again and push into [flatNames]
     results.forEach((result) => {
-      console.error(result, 'result');
       const singleArray = result.flat();
-      const names = [];
       singleArray.forEach((obj) => { if (obj) names.push(obj.name); });
       const newNames = Object.keys(names).map((key) => [names[key]]);
       const newFlatNames = newNames.flat();
       flatNames.push(newFlatNames);
-      console.error(counts, 'counts in');
     });
-    console.error(flatNames, 'flatNames');
+    // Now we have to flatten AGAIN and store them an array called [bigFlatNames]
+    // then we count all the similar names and store them in an object with the name as the key and its count as the value
     const bigFlatNames = flatNames.flat();
-    console.error(bigFlatNames);
     bigFlatNames.forEach((x) => { counts[x] = (counts[x] || 0) + 1; });
-    console.error(counts, 'counts');
+    // Now we have to iterate through the object we just created and display the ingredient name along with its count
     Object.entries(counts).forEach((count) => {
       const key = count[0];
       const value = count[1];
@@ -36,6 +39,7 @@ const getIngredientsReport = () => new Promise((resolve, reject) => {
       domString += '</div>';
       console.error(count, 'count');
     });
+    // print the cards to the DOM!
     utils.printToDom('ingredient-reporting-section', domString);
   }).catch((err) => reject(err));
 });
