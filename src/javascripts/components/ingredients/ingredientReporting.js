5d8a541eb867038787f06f248ec2de3437a566b6
Aaron Lain <aaron.j.lain@gmail.com>
2020-05-02 10:57:00 -0500

    ingredients reporting works bruh

--- src/javascripts/components/ingredients/ingredientReporting.js
+++ src/javascripts/components/ingredients/ingredientReporting.js
@@ -2,6 +2,12 @@ import moment from 'moment';
 import utils from '../../helpers/utils';
 import smash from '../../helpers/data/smashData';
 
+const resetFormValues = () => {
+  $('#date1').val('');
+  $('#date2').val('');
+  $('#date3').val('');
+};
+
 const getProperArray = (start, end) => new Promise((resolve, reject) => {
   const names = [];
   const counts = {};
@@ -19,12 +25,19 @@ const getProperArray = (start, end) => new Promise((resolve, reject) => {
 const getIngredientsReport = () => new Promise((resolve, reject) => {
   const date1 = $('#date1').val();
   const date2 = $('#date2').val();
-  const modifiedDate1 = moment(date1).format('YYYY/MM/DD');
-  const modifiedDate2 = moment(date2).format('YYYY/MM/DD');
+  const date3 = $('#date3').val();
+  let modifiedDate1 = '';
+  let modifiedDate2 = '';
+  if (date3 === '') {
+    modifiedDate1 = moment(date1).format('YYYY/MM/DD');
+    modifiedDate2 = moment(date2).format('YYYY/MM/DD');
+  } else {
+    modifiedDate1 = moment(date3).format('YYYY/MM/DD');
+    modifiedDate2 = moment(date3).format('YYYY/MM/DD');
+  }
   let domString = '';
   getProperArray(modifiedDate1, modifiedDate2).then((results) => {
-    domString += `<h4 class="col-12" style="text-align: center;">Inventory Used from 
-    ${moment(modifiedDate1).format('MMM Do YYYY')} and ${moment(modifiedDate2).format('MMM Do YYYY')}</h4>`;
+    domString += '<h4 class="col-12" style="text-align: center;">Inventory Used</h4>';
     Object.entries(results).forEach((result) => {
       const key = result[0];
       const value = result[1];
@@ -36,6 +49,7 @@ const getIngredientsReport = () => new Promise((resolve, reject) => {
     });
     utils.printToDom('ingredient-reporting-section', domString);
     resolve(results);
+    resetFormValues();
   }).catch((err) => reject(err));
 });
 
